31a30a36069df41c99f324c65beb815ca98a09de
김민지 <ghi512151@gmail.com>
2024-06-26 20:40:07 +0900

    내용 구성
    https://create-react-app.dev/ 사이트 내용 참고

--- src/App.js
+++ src/App.js
@@ -4,7 +4,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Hello React!
+      <header>
+        <h1><a href="/">CREATE REACT APP</a></h1>
+      </header>
+      <nav>
+        <ol>
+          <li><a href="/read/1">Less to Learn</a></li>
+          <li><a href="/read/2">Only One Dependency</a></li>
+          <li><a href="/read/3">No Lock-In</a></li>
+        </ol>
+      </nav>
+      <article>
+        <h1>REACT site</h1>
+        <a href='https://create-react-app.dev/'>https://create-react-app.dev/</a>
+      </article>
     </div>
   );
 }
